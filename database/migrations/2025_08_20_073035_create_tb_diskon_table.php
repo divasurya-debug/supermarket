cf8822856a0ca9df2c047601cc74f5b63a60f43e
Nidhom13 <enmageming@gmail.com>
2025-08-20 14:39:49 +0700

    revisi tb_diskon

--- database/migrations/2025_08_20_073035_create_tb_diskon_table.php
+++ database/migrations/2025_08_20_073035_create_tb_diskon_table.php
@@ -10,9 +10,13 @@ return new class extends Migration
     {
         Schema::create('tb_diskon', function (Blueprint $table) {
             $table->id('id_diskon');                         // PK
-            $table->foreignId('id_produk')                   // relasi ke produk
-                  ->constrained('tb_produk')
+
+            // perbaikan foreign key
+            $table->unsignedBigInteger('id_produk');         
+            $table->foreign('id_produk')
+                  ->references('id_produk')->on('tb_produk')
                   ->onDelete('cascade');
+
             $table->integer('persentase_diskon');            // persentase diskon
             $table->date('tanggal_mulai');                   // tanggal mulai
             $table->date('tanggal_akhir');                   // tanggal akhir
