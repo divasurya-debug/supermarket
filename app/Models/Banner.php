13697b7427d9c2a8be27338f6eca15893d21512a
Nidhom13 <enmageming@gmail.com>
2025-08-21 15:49:23 +0700

    nyambung tb_banner

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -9,19 +9,12 @@ class Banner extends Model
 {
     use HasFactory;
 
-    // Nama tabel di database
     protected $table = 'tb_banner';
-
-    // Primary key tabel
     protected $primaryKey = 'id_banner';
 
-    // Jika primary key bukan auto-increment integer, baru set ini:
-    // public $incrementing = false;
-    // protected $keyType = 'string';
-
-    // Kolom yang bisa diisi mass-assignment
+    // Kolom yang bisa diisi
     protected $fillable = ['gambar'];
 
-    // Jika tabel tidak punya kolom created_at & updated_at
-    public $timestamps = false;
+    // timestamps aktif karena ada created_at & updated_at
+    public $timestamps = true;
 }
