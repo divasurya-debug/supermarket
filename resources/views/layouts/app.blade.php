f59699d26d5d0ddb1c3b0055622f1cb4b8eb8573
divasurya <divasurya628@gmail.com>
2025-09-19 12:41:46 +0700

    d

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Klik Supermarket</title>
+    <title>@yield('title', 'Klik Supermarket')</title>
+
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    
     <!-- Bootstrap Icons -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
 
@@ -18,11 +21,12 @@
 </head>
 <body class="bg-light">
 
-    <!-- Content -->
-   <main class="p-0 m-0">
+    <!-- Wrapper utama -->
+    <main class="p-0 m-0">
         @yield('content')
     </main>
 
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
