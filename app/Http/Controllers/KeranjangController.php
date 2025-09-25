e5e11760e5c88f525210d68e06597a169372d757
divasurya <divasurya628@gmail.com>
2025-09-25 14:28:05 +0700

    co

---
+++ app/Http/Controllers/KeranjangController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Produk; // pastikan model Produk ada di App\Models
+
+class KeranjangController extends Controller
+{
+    /**
+     * Tampilkan isi keranjang
+     */
+    public function index(Request $request)
+    {
+        $keranjang = $request->session()->get('keranjang', []);
+        return view('frontend.keranjang.index', compact('keranjang'));
+    }
+
+    /**
+     * Tambah produk ke keranjang
+     */
+    public function add(Request $request, $id)
+    {
+        $produk = Produk::findOrFail($id);
+
+        $keranjang = $request->session()->get('keranjang', []);
+
+        if (isset($keranjang[$id])) {
+            $keranjang[$id]['jumlah']++;
+        } else {
+            $keranjang[$id] = [
+                'nama_produk' => $produk->nama_produk,
+                'harga'       => $produk->harga,
+                'gambar'      => $produk->gambar,
+                'jumlah'      => 1,
+            ];
+        }
+
+        $request->session()->put('keranjang', $keranjang);
+
+        return redirect()
+            ->route('keranjang.index')
+            ->with('success', $produk->nama_produk . ' berhasil ditambahkan ke keranjang!');
+    }
+
+    /**
+     * Hapus produk dari keranjang
+     */
+    public function remove(Request $request, $id)
+    {
+        $keranjang = $request->session()->get('keranjang', []);
+
+        if (isset($keranjang[$id])) {
+            unset($keranjang[$id]);
+            $request->session()->put('keranjang', $keranjang);
+        }
+
+        return redirect()
+            ->route('keranjang.index')
+            ->with('success', 'Produk berhasil dihapus dari keranjang!');
+    }
+}
