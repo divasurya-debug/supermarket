e750c989bf00d432f0d342359ad29fba7476eef2
divasurya <divasurya628@gmail.com>
2025-09-26 11:39:22 +0700

    f

--- app/Http/Controllers/PromoController.php
+++ app/Http/Controllers/PromoController.php
@@ -4,17 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Discount;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class PromoController extends Controller
 {
     public function index()
     {
-        // Ambil data promo/diskon terbaru dengan relasi produk
+        // Ambil tanggal hari ini
+        $today = Carbon::today();
+
+        // Ambil data promo aktif saja (tanggal_akhir >= hari ini)
         $promo = Discount::with('product')
+            ->whereDate('tanggal_akhir', '>=', $today)
             ->orderBy('created_at', 'desc')
             ->paginate(12);
 
-        // Panggil view di folder frontend/promo/index.blade.php
+        // Tampilkan view promo
         return view('frontend.promo.index', compact('promo'));
     }
 }
